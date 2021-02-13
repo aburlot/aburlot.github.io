5ecd43865796af85f5c64090447bfe6130d69bb3
Alan <alan.burlot@protonmail.com>
2021-02-13 20:49:02 +0100

    fin bartok + modif sw

--- js/main.js
+++ js/main.js
@@ -16,3 +16,22 @@ window.onload = () => {
              .register('/sw.js');
   }
 }
+
+// Test du cache https://web.dev/codelab-http-cache/
+const express = require('express');
+const app = express();
+app.use(express.static('public', {
+  etag: true, // Just being explicit about the default.
+  lastModified: true,  // Just being explicit about the default.
+  setHeaders: (res, path) => {
+    const hashRegExp = new RegExp('\\.[0-9a-f]{8}\\.');
+
+    if (path.endsWith('.html')) {
+      // All of the project's HTML files end in .html
+      res.setHeader('Cache-Control', 'no-cache');
+    } else if (hashRegExp.test(path)) {
+      // If the RegExp matched, then we have a versioned URL.
+      res.setHeader('Cache-Control', 'max-age=31536000');
+    }
+  },
+}));
